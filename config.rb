c16a36551a9f8a8050a19204561812cfed4cf6fb
gajubhat <gajukbhat@gmail.com>
2013-12-01 13:20:42 +0530

    Setting the correct paths for compass

--- config.rb
+++ config.rb
@@ -11,9 +11,9 @@ http_fonts_path = "/fonts"
 css_dir = "public/stylesheets"
 
 # Local development paths
-sass_dir = "sass"
+sass_dir = "source/_assets/stylesheets"
 images_dir = "source/_assets/images"
-fonts_dir = "source/fonts"
+fonts_dir = "source/_fonts"
 
 line_comments = false
 output_style = :compressed
